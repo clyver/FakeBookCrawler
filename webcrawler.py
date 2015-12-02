4bb88c4333df6c1021ac571ff03e47e14ea441f4
Christopher M Lyver <lyverc@cloche.ccs.neu.edu>
2015-12-02 16:41:39 -0500

    Lots of print statements, some formating.  Can't Auth, but methods themselves are good...I think

--- webcrawler.py
+++ webcrawler.py
@@ -35,20 +35,40 @@ username = '001180021'
 passwd = 'E1ELONFK'
 
 
-def get(url):
+def get(url, cookies=None):
     """
     Make a GET request to the given url
     """
-
+    sock = open_socket()
     parsed_url = urlparse.urlparse(url)
     path = parsed_url.path
     if not path:
         path = "/"
 
     # Fire away
-    sock.send("GET {} HTTP/1.0{}".format(path, crlf))
-    data = sock.recv(1000000)
-    return repr(data)
+    get_request = "GET {} HTTP/1.0{}".format(path, crlf)
+    if cookies:
+        csrf_token = cookies.get('csrf_token')
+        session_id = cookies.get('session_id')
+        get_request += "Set-Cookie: sessionid={}; csrftoken={}{}".format(session_id, csrf_token, crlf)
+
+        get_request = "POST {} HTTP/1.0\{}".format(path, crlf) + \
+                        "Host:fring.ccs.neu.edu:80\r\n" + \
+        	            "Referer: http://fring.ccs.neu.edu/accounts/login/?next=/fakebook/\r\n" + \
+        	            "Set-Cookie:csrftoken="+csrf_token+"; sessionid="+session_id+"\r\n" + \
+        	            "Connection:keep-alive\r\n" + \
+        	            "Content-Type: application/x-www-form-urlencoded\r\n"
+    print "******Sending GET Request:"
+    print get_request
+    sock.send(get_request)
+    data = sock.recv(4096)
+
+    sock.shutdown(1)
+    sock.close()
+    print "Get request responded with:  ******************"
+    print data
+    return data
+
 
 
 def post(url, params):
@@ -58,10 +78,22 @@ def post(url, params):
     csrf_token = params.get('csrfmiddlewaretoken')
     sess_id = params.get('sessionid')
 
-    final_message = "POST /accounts/login/ HTTP/1.0\r\n" + "Host:fring.ccs.neu.edu:80\r\nReferer: http://fring.ccs.neu.edu/accounts/login/?next=/fakebook/\r\nCookie:csrftoken="+csrf_token+"; sessionid="+sess_id+"\r\nConnection:keep-alive\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length:109\r\n\r\ncsrfmiddlewaretoken="+csrf_token+"&username="+username+"&password="+passwd+"&next=%2Ffakebook%2F\r\n\r\n"
+    new_sock = open_socket()
+    http_params = "csrfmiddlewaretoken="+csrf_token+"&username="+username+"&password="+passwd+"&next=%2Ffakebook%2F"
+    params_len = len(http_params)
+    final_message = "POST /accounts/login/ HTTP/1.0\r\n" + \
+                    "Host:fring.ccs.neu.edu:80\r\n" + \
+		            "Referer: http://fring.ccs.neu.edu/accounts/login/?next=/fakebook/\r\n" + \
+		            "Cookie:csrftoken="+csrf_token+"; sessionid="+sess_id+"\r\n" + \
+		            "Connection:keep-alive\r\n" + \
+		            "Content-Type: application/x-www-form-urlencoded\r\n" + \
+		            "Content-Length:{}\r\n\r\n".format(params_len) + \
+                    http_params + "\r\n\r\n"
+    print "Sending the following post request:"
     print final_message
-    sock.sendall(final_message)
-    response = sock.recv(1000000)
+    new_sock.sendall(final_message)
+    response = new_sock.recv(4096)
+    print "Response**************"
     print response
     return response
 
@@ -71,14 +103,7 @@ def open_socket():
     Establish and return a tcp socket
     """
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    return s
-
-
-def connect_socket(s, address, port):
-    """
-    Return a socket that is connected to the given address/port
-    """
-    s.connect((address, port))
+    s.connect((target_host, http_port))
     return s
 
 
@@ -162,24 +187,22 @@ def get_cookies():
 
 
 def login():
+    print "Getting cookies..."
     csrf_cookie, session_id = get_cookies()
+    print "*******Here are the cookies found - received csrf: {} and sessionId: {}".format(csrf_cookie, session_id)
     params = {'username': username,
               'password': passwd,
               'csrfmiddlewaretoken': csrf_cookie,
               'sessionid': session_id,
               'next': '/fakebook/'}
-    post(target_login, params)
+    print "*******Now logging in with those creds"
+    auth_response = post(target_login, params)
+    # The login page returns a new session id that we'll need moving forward
+    cookie_jar = fetch_patterns(r"Set-Cookie\:(?:(?!;)(?:.|\n))*;", auth_response)
+    final_session_id = cookie_jar[0].split('=')[1].split(';')[0]
+    print "Our final session id {}".format(final_session_id)
+    print "Going for the money..."
+    res = get(target_domain, {'session_id': final_session_id, 'csrf_token': csrf_cookie})
 
-# We need one connection
-# Establish and connect our socket
-sock = open_socket()
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-host_ip = socket.gethostbyname(target_host)
-sock = connect_socket(sock, host_ip, http_port)
 
 login()
-# Close up shop...
-sock.shutdown(1)
-sock.close()
-
-
